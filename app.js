dd210ab90d8ab8a629b6a60bd2b486ac7043eb34
SanSaich <kurdenkov93@gmail.com>
2021-10-16 16:19:39 +0700

    перебор объекта методом forEach

--- app.js
+++ app.js
@@ -10,5 +10,17 @@ async function uploadFile(input) {
   let fileJson = await input.files[0].text();
   let objJson = JSON.parse(fileJson);
   resetPage(fileJson);
-  console.log(objJson);
+
+  forObj(objJson);
 }
+
+const forObj = (obj) => {
+  Object.entries(obj).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      console.log("Массив:", key);
+      forObj(value);
+    } else {
+      console.log("Ключ:", key, "Значение:", value);
+    }
+  });
+};
